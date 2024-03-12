dd1817f9bd419e807429c1ad92959b889bf1e167
ponikarovskii <ponikarovskii.anton@gmail.com>
2024-03-12 14:00:18 +0100

    Initial Commit

---
+++ father_hit_task_5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  father_hit_task_5
+//
+//  Created by Антон Поникаровский on 12.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ father_hit_task_5/father_hit_task_5App.swift
@@ -0,0 +1,17 @@
+//
+//  father_hit_task_5App.swift
+//  father_hit_task_5
+//
+//  Created by Антон Поникаровский on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct father_hit_task_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
